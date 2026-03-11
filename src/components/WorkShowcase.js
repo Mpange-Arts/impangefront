d323d26cb8dd70365cd988ecf490f8d3051fff40
Precious3366 <preciousbanda3366@gmail.com>
2026-03-11 20:24:30 +0200

    adjustment eslint issue

--- src/components/WorkShowcase.js
+++ src/components/WorkShowcase.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { motion, AnimatePresence } from 'framer-motion';
-import ParallaxImage from './ParallaxImage';
 
 // --- Project Data ---
 const PROJECTS = [
